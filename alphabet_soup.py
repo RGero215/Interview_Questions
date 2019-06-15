c22c40b9fca6031af2443edc2073dce9c9b2153e
Ramon Geronimo <rgero215@gmail.com>
2019-06-15 15:15:32 -0700

    add clarifying questions, assumptions, proccessing thinking and rationale

--- alphabet_soup.py
+++ alphabet_soup.py
@@ -2,13 +2,33 @@
 # characters in a string. We'll sort the characters using
 # the built-in array sort function.
 
+''' 
+Clarifying question:
+    do I have to create a sorted function or can I use 
+    sorted method?
+
+Assumptions:
+    input can be a sentences or a single word
+    we can use sorted method
+
+Thinking out laoud:
+  If we use sorted to arrange the characters in a word
+  we just need to turned the string into an array of 
+  chars and return it.
+
+Rationale:
+  convert the string into a list of characters
+  sort the list in alphabetical order
+  return the newly joined string 
+'''
+
 print('''
 Enter word to get sorted characters.
 input: "Monday" 
 output: Madnoy
 ''')
 def alphabetSoup(str):
-    chars = list(str) # turn string into list of characters
+    chars = list(str)
     sortedChars = sorted(chars)
     return ''.join(sortedChars)
 
--- first_reverse.py
+++ first_reverse.py
@@ -1,6 +1,27 @@
 # This challenge requires you to reverse an input string. 
+''' 
+Clarifying question:
+    is the input only a single word?
+    if not, do I have to handle black space?
+    can I used reversed method?
+
+Assumptions:
+    input can be a sentences or a single word
+    can't use reversed
+
+Thinking out laoud:
+  the easiest way to reverse a string in python is 
+  treating the string as an array and appending the character in it.
+
+Rationale:
+  The colons inside represent str[start:stop:step] where if step is a negative number
+  it'll loop through the string backwards 
+'''
+
+
 print('''Enter word in single quotes or double quotes to be reverse. 
 Ex. input: "Monday" output: yadnoM''')
+
 def FirstReverse(str): 
     return str[::-1]
 
