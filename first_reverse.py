63729fa8740f799745a98b4b912986242e257a4a
Ramon Geronimo <rgero215@gmail.com>
2019-06-15 14:23:24 -0700

    add first reverse

---
+++ first_reverse.py
@@ -0,0 +1,7 @@
+# This challenge requires you to reverse an input string. 
+print('''Enter word in single quotes or double quotes to be reverse. 
+Ex. input: "Monday" output: yadnoM''')
+def FirstReverse(str): 
+    return str[::-1]
+
+print(FirstReverse(input()))
